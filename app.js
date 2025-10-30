ef2cd7f34849b7e9fe09e31328ab9847695f5b95
khalilsediq <khalilullahsadique1334@gmail.com>
2025-10-30 09:28:44 +0500

    Array Assignment

---
+++ app.js
@@ -0,0 +1,15 @@
+
+// let array = ["Khalil, wali, rafi"];
+
+// console.log(array);
+// array.push("Subhan");
+
+
+// let fruits = ["banana, Manngo, Water-Melon", "pomegranate", "Apple", "Guava"];
+// fruits.pop()
+// console.log(fruits);
+
+// let days = ["Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+// days.unshift("Monday")
+// console.log(days);
+
