762f8dec6bbad1241aea435b8593e0290d43feae
khalilsediq <khalilullahsadique1334@gmail.com>
2025-10-31 09:31:54 +0500

    done

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
+let result = document.querySelector("#result");
 
-// let array = ["Khalil, wali, rafi"];
+function onResult(){
+    let array = ["Khalil, wali, rafi"];
+    array.push("Subhan");
+    result.innerHTML = array;
+    console.log(array);
+}
 
-// console.log(array);
-// array.push("Subhan");
+function onResult(){
+let fruits = ["banana, Manngo, Water-Melon", "pomegranate", "Apple", "Guava"];
+fruits.pop();
+result.innerHTML = fruits;
+console.log(fruits);
+// result.innerHTML = ""
+}
 
+function onResult(){
+    let days = ["Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    days.unshift("Monday")
+result.innerHTML = days;
+console.log(days);
+}
 
-// let fruits = ["banana, Manngo, Water-Melon", "pomegranate", "Apple", "Guava"];
-// fruits.pop()
-// console.log(fruits);
-
-// let days = ["Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-// days.unshift("Monday")
-// console.log(days);
-
-
-
+function onResult(){
 let colors = ['red', 'green', 'orange', 'teal', 'blue', 'purple'];
-
 colors.shift();
+result.innerHTML = colors;
 console.log(colors);
+}
